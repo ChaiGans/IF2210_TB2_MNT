c4584fe8761f3c79e311a5583fb99cf856a8f879
ChaiGans <elbertchailes888@gmail.com>
2024-05-19 06:11:36 +0700

    feat: gamestate entity (not yet configuration)

---
+++ src/src/main/java/entity/GameState.java
@@ -0,0 +1,42 @@
+package entity;
+
+import java.util.*;
+
+public class GameState {
+    private List<Player> players;
+    private int currentTurn;
+    private Store store;
+    private GameSettings settings; // Another class representing configurable settings.
+
+    public GameState() {
+        this.currentTurn = 0;
+        this.store = new Store();
+        players = new ArrayList<>();
+        settings = new GameSettings();
+    }
+
+    // Getters and setters for each field
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public GameSettings getSettings() {
+        return settings;
+    }
+
+    public void setSettings(GameSettings settings) {
+        this.settings = settings;
+    }
+    
+    public Store getStore() {
+        return this.store;
+    }
+
+    public int getCurrentTurn() {
+        return this.currentTurn;
+    }
+}
