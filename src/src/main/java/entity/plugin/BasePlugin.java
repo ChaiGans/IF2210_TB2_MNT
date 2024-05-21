f5ab9cca0dbca8b2a4be6a6c2276af4d8aacb8bb
ChaiGans <elbertchailes888@gmail.com>
2024-05-21 20:09:59 +0700

    add: missed conversion

--- src/src/main/java/entity/plugin/BasePlugin.java
+++ src/src/main/java/entity/plugin/BasePlugin.java
@@ -9,23 +9,35 @@ public abstract class BasePlugin {
 
     public BasePlugin() {
         this.keyToValueMap.put("SIRIP_HIU", "Shark Fin");    
-        this.keyToValueMap.put("Shark Fin", "SIRIP_HIU");    
+        this.keyToValueMap.put("Shark Fin", "SIRIP_HIU");
+        this.keyToValueMap.put("HIU_DARAT", "Land Shark");
+        this.keyToValueMap.put("Land Shark", "HIU_DARAT");
         this.keyToValueMap.put("SUSU", "Milk");    
-        this.keyToValueMap.put("Milk", "SUSU");    
+        this.keyToValueMap.put("Milk", "SUSU");
+        this.keyToValueMap.put("SAPI", "Cow");
+        this.keyToValueMap.put("Cow", "SAPI");
         this.keyToValueMap.put("DAGING_DOMBA", "Sheep Meat");    
         this.keyToValueMap.put("Sheep Meat", "DAGING_DOMBA");    
         this.keyToValueMap.put("DAGING_KUDA", "Horse Meat");    
-        this.keyToValueMap.put("Horse Meat", "DAGING_KUDA");    
+        this.keyToValueMap.put("Horse Meat", "DAGING_KUDA");
+        this.keyToValueMap.put("KUDA", "Horse");
+        this.keyToValueMap.put("Horse", "KUDA");
         this.keyToValueMap.put("DAGING_BERUANG", "Bear Meat");    
         this.keyToValueMap.put("Bear Meat", "DAGING_BERUANG");    
+        this.keyToValueMap.put("BERUANG", "Bear");    
+        this.keyToValueMap.put("Bear", "BERUANG");    
         this.keyToValueMap.put("TELUR", "Egg");    
-        this.keyToValueMap.put("Egg", "TELUR");    
+        this.keyToValueMap.put("Egg", "TELUR");
+        this.keyToValueMap.put("AYAM", "Chicken");
+        this.keyToValueMap.put("Chicken", "AYAM");
         this.keyToValueMap.put("STROBERI", "Strawberry");    
         this.keyToValueMap.put("Strawberry", "STROBERI");    
         this.keyToValueMap.put("LABU", "Pumpkin");    
         this.keyToValueMap.put("Pumpkin", "LABU");    
         this.keyToValueMap.put("JAGUNG", "Corn");    
-        this.keyToValueMap.put("Corn", "JAGUNG");    
+        this.keyToValueMap.put("Corn", "JAGUNG");
+        this.keyToValueMap.put("DOMBA", "Sheep");
+        this.keyToValueMap.put("Sheep", "DOMBA");
         this.keyToValueMap.put("ACCELERATE", "Accelerate");    
         this.keyToValueMap.put("DELAY", "Delay");    
         this.keyToValueMap.put("PROTECT", "Protect");    
@@ -37,6 +49,12 @@ public abstract class BasePlugin {
         this.keyToValueMap.put("Protect", "PROTECT");    
         this.keyToValueMap.put("Destroy", "DESTROY");    
         this.keyToValueMap.put("Trap", "TRAP");    
-        this.keyToValueMap.put("Instant Harvest", "INSTANT_HARVEST");    
+        this.keyToValueMap.put("Instant Harvest", "INSTANT_HARVEST");
+        this.keyToValueMap.put("BIJI_LABU", "Pumpkin Seed");
+        this.keyToValueMap.put("Pumpkin Seed", "BIJI_LABU");
+        this.keyToValueMap.put("BIJI_JAGUNG", "Corn Seed");
+        this.keyToValueMap.put("Corn Seed", "BIJI_JAGUNG");
+        this.keyToValueMap.put("BIJI_STROBERI", "Strawberry Seed");
+        this.keyToValueMap.put("Strawberry Seed", "BIJI_STROBERI");
     }
 }
